7e33acc66da458bc9ddd842f26a72b6ee00939cc
felipebuso <felipe.buso@hotmail.com>
2025-04-07 15:55:22 -0300

    inicio dos estudos

---
+++ src/main/java/dio/spring_web/AulaSpringWebSwaggerApplication.java
@@ -0,0 +1,13 @@
+package dio.spring_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaSpringWebSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaSpringWebSwaggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/spring_web/AulaSpringWebSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.spring_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaSpringWebSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
