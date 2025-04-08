e665d0bd0fb5ec75ec4286439a9cd26eb0676e22
felipebuso <felipe.buso@hotmail.com>
2025-04-08 09:17:28 -0300

    primeiro controller

---
+++ src/main/java/dio/spring_web/controller/UsuarioController.java
@@ -0,0 +1,37 @@
+package dio.spring_web.controller;
+
+import dio.spring_web.model.Usuario;
+import dio.spring_web.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("users")
+public class UsuarioController {
+    @Autowired
+    private UsuarioRepository repository;
+
+    @GetMapping()
+    public List<Usuario> getUsers() {
+        return repository.listAll();
+    }
+
+    @GetMapping("/{username}")
+    public Usuario getOne(@PathVariable("username") String username) {
+        return repository.findByUsername(username);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable("id") Integer id) {
+        repository.remove(id);
+    }
+
+    @PostMapping()
+    public void postUser(@RequestBody Usuario usuario) {
+        repository.save(usuario);
+    }
+}
